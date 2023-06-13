391581a8b7913a943abfd42030801bd30ba8128a
Dennis Martinez <dennis@dennmart.com>
2023-06-13 17:14:31 +0900

    [WIP] First pass at moving processed videos to a new bucket

--- lambda/media_convert_job_completed/lambda.mjs
+++ lambda/media_convert_job_completed/lambda.mjs
@@ -1,5 +1,46 @@
+import {
+  MediaConvertClient,
+  GetJobCommand,
+} from "@aws-sdk/client-mediaconvert";
+import {
+  S3Client,
+  CopyObjectCommand,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+import mediaConvertEndpoint from "./mediaConvertEndpoint.mjs";
+
 export const handler = async (event) => {
-  console.log(JSON.stringify(event));
+  // console.log(JSON.stringify(event));
+
+  const endpoints = await mediaConvertEndpoint();
+  const config = {
+    region: "ap-northeast-1",
+    endpoint: endpoints.Endpoints[0].Url,
+  };
+
+  const client = new MediaConvertClient(config);
+  const command = new GetJobCommand({ Id: event.detail.jobId });
+  const response = await client.send(command);
+
+  const inputFile = response.Job.Settings.Inputs[0].FileInput;
+
+  const s3Client = new S3Client({ region: "ap-northeast-1" });
+  const copyCommand = new CopyObjectCommand({
+    CopySource: inputFile.replace("s3://", ""),
+    Bucket: "video-clipper-input", //process.env.PROCESSED_BUCKET,
+    Key: `processed-videos/${inputFile.split("/").pop()}`,
+  });
+  const s3Response = await s3Client.send(copyCommand);
+
+  const deleteCommand = new DeleteObjectCommand({
+    Bucket: "video-clipper-input", //process.env.PROCESSED_BUCKET,
+    Key: inputFile.split("/").pop(),
+  });
+  await s3Client.send(deleteCommand);
+
+  // 4. Copy the input file into the processed videos folder
+  // 5. Delete the original input file
+
   return {
     statusCode: 200,
     body: JSON.stringify(event),
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -12,21 +12,32 @@ data "aws_iam_policy_document" "assume_lambda_role" {
   }
 }
 
-# IAM role to set on our Lambda function.
+# IAM role to set on our Lambda input function.
 resource "aws_iam_role" "lambda_iam_role" {
   name               = "video-clipper-lambda-iam-role"
   assume_role_policy = data.aws_iam_policy_document.assume_lambda_role.json
 }
 
-# Attach the AWSLambdaBasicExecutionRole to allow Lambda function to
+# IAM role to set on our Lambda job complete function.
+resource "aws_iam_role" "lambda_job_completed_role" {
+  name               = "VideoClipperLambdaJobCompleteRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_lambda_role.json
+}
+
+# Attach the AWSLambdaBasicExecutionRole to allow Lambda functions to
 # write logs to CloudWatch.
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.lambda_iam_role.name
 }
 
-# Policy document to allow Lambda to access our S3 buckets to get file info,
-# pass a role, and perform MediaConvert actions.
+resource "aws_iam_role_policy_attachment" "lambda_job_complete_basic_execution" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_job_completed_role.name
+}
+
+# Policy document to allow the Lambda input function to access our S3 buckets
+# to get file info, pass a role, and perform MediaConvert actions.
 data "aws_iam_policy_document" "lambda_policy_document" {
   statement {
     effect = "Allow"
@@ -77,7 +88,7 @@ data "aws_iam_policy_document" "lambda_policy_document" {
   }
 }
 
-# Policy to attach to the Lambda IAM role.
+# Policy to attach to the Lambda input IAM role.
 resource "aws_iam_policy" "lambda_role_policy" {
   name   = "video-clipper-lambda-role-policy"
   policy = data.aws_iam_policy_document.lambda_policy_document.json
@@ -87,6 +98,68 @@ resource "aws_iam_policy" "lambda_role_policy" {
 resource "aws_iam_role_policy_attachment" "attach_lambda_policy" {
   role       = aws_iam_role.lambda_iam_role.name
   policy_arn = aws_iam_policy.lambda_role_policy.arn
+}
+
+# Policy document to allow the Lambda job complete function to perform
+#  MediaConvert actions and access our S3 input bucket.
+
+data "aws_region" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "lambda_job_complete_policy_document" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:CopyObject",
+      "s3:DeleteObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.input_bucket.arn}/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:PassRole"
+    ]
+    resources = [
+      aws_iam_role.mediaconvert_iam_role.arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "mediaconvert:DescribeEndpoints"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "mediaconvert:GetJob"
+    ]
+    resources = [
+      "arn:${data.aws_partition.current.partition}:mediaconvert:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:jobs/*"
+    ]
+  }
+}
+
+# Policy to attach to the Lambda job completed IAM role.
+resource "aws_iam_policy" "lambda_job_completed_role_policy" {
+  name   = "VideoClipperLambdaJobCompletedRolePolicy"
+  policy = data.aws_iam_policy_document.lambda_job_complete_policy_document.json
+}
+
+# Attach the Lambda policy to the IAM role for the Lambda job completed function.
+resource "aws_iam_role_policy_attachment" "attach_lambda_job_completed_policy" {
+  role       = aws_iam_role.lambda_job_completed_role.name
+  policy_arn = aws_iam_policy.lambda_job_completed_role_policy.arn
 }
 
 # Policy document to allow access to MediaConvert.
